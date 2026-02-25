e4ee70420bcf89a225a8d5c588d848d741f92ed4
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-02-24 21:56:57 -0300

    att

--- POO/desafios/etiqueta.py
+++ POO/desafios/etiqueta.py
@@ -10,13 +10,17 @@ class produto:
         self.preco = preco
 
     def etiqueta(self):
-        caixa = Panel(f"{self.nome}\nPreço: R${self.preco:.2f}", title='Produto', width=30)
+        self.precof = f"Preço: R${self.preco:.2f}"
+
+        caixa = Panel(f"{self.nome.center(29, ' ')}  {self.precof.center(30, '-')}", title='Produto', width=35)
         print(caixa)
         
     
-p1 = produto(nome='Iphone', preco=1200)
+p1 = produto(nome='Iphone 17 pro Max', preco=7200)
 p2 = produto(nome='Xbox Series S', preco=2500)
+p3 = produto(nome='Notebook Gamer', preco=5000)
 
 p1.etiqueta()
 p2.etiqueta()
+p3.etiqueta()
 
