d55f7729d361a70007aeac84aa2737d23a6c0e0c
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-03-02 20:26:09 -0300

    att

--- POO/desafios/churras.py
+++ POO/desafios/churras.py
@@ -26,10 +26,13 @@ class churrasco:
         self.ValorTotal = self.kg_total * 82.40
         self.ValorDividido = self.ValorTotal / self.quant
 
-        caixa = Panel(f"Analisando [green]Churras dos Amigos[/] com [blue]{self.quant} convidados[/]\nCada participante comerá 0.400g e cada Kg custa R$82.40\nRecomendo [blue]comprar {self.kg_total:.3f}Kg[/] de carne\nO custo total será de [green]R${self.ValorTotal:.2f}[/]\nCada pessoa pagará [red]R${self.ValorDividido:.2f}[/] para participar.", title='Churras dos Amigos', expand=False)
+        caixa = Panel(f"Analisando [green]{self.titulo}[/] com [blue]{self.quant} convidados[/]\nCada participante comerá 0.400g e cada Kg custa R$82.40\nRecomendo [blue]comprar {self.kg_total:.3f}Kg[/] de carne\nO custo total será de [green]R${self.ValorTotal:.2f}[/]\nCada pessoa pagará [red]R${self.ValorDividido:.2f}[/] para participar.", title=self.titulo, expand=False)
 
         print(caixa)
 
 
 c1 = churrasco(titulo='Churras dos Amigos', quant=15)
 c1.analisar()  # 08:20
+
+c2 = churrasco(titulo='Festa Fim de Ano', quant=80)
+c2.analisar()
