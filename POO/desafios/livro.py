a849c876498e0ad256b8ea3440ae0ccbb8234d71
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-03-09 20:49:14 -0300

    att

--- POO/desafios/livro.py
+++ POO/desafios/livro.py
@@ -1,4 +1,5 @@
 from rich import print
+from time import sleep
 
 class livro:
     def __init__(self, titulo, paginas):
@@ -8,18 +9,24 @@ class livro:
 
         print(f"Você acabou de abrir o livro '{self.titulo}' que tem {self.paginas} paginas no total. Você agora está na página {self.pagina_atual}")
 
-    def avançar_paginas(self, n):
+    def avançar_paginas(self, n=1):
+        
         cont = 0
         while cont < n and self.pagina_atual < self.paginas:
             print(f'pag {self.pagina_atual+1}', end= ' > ')
+            sleep(0.2)
             self.pagina_atual += 1
             cont += 1
 
-        print(f'[blue]Você avançou {n} paginas e agora esta na [red]pagina {self.pagina_atual}')
+
+        sleep(0.5)
+        print(f'[blue]Você avançou {n} paginas e agora esta na [green]pagina {self.pagina_atual}')
+
+        if self.pagina_atual == self.paginas:
+            print('[red]Voce chegou no final do livro!')
 
 
 l1 = livro(titulo='10 coisas que aprendi', paginas=20)
-l1.avançar_paginas(6)
-l1.avançar_paginas(5)
-l1.avançar_paginas(10)
-                    #11:30
+l1.avançar_paginas(30)
+
+                
