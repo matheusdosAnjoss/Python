b1e2342f32377110a0a86de0639d484b73d3ee69
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-03-31 23:38:01 -0300

    terminei o projeto controle.py

--- POO/desafios/controle.py
+++ POO/desafios/controle.py
@@ -1,7 +1,21 @@
 from rich import print
 from rich.panel import Panel
 
+"""
+Módulo de controle remoto simulado para uma TV simples usando Rich para UI.
+Contém a classe ControleRemoto e um loop interativo para testar comandos.
+"""
+
 class ControleRemoto:
+    """
+    Classe que representa um controle remoto para TV.
+    
+    Atributos de classe:
+        canal_min (int): Canal mínimo disponível (padrão 1).
+        canal_max (int): Canal máximo disponível (padrão 6).
+        volume_min (int): Volume mínimo (padrão 1).
+        volume_max (int): Volume máximo (padrão 5).
+    """
     canal_min: int = 1
     canal_max: int = 6
     volume_min: int = 1
@@ -21,7 +35,7 @@ class ControleRemoto:
             if self.canal_atual == self.canal_max:
                 self.canal_atual = self.canal_min
             else:
-                self.canal_atual += 1 #15min esta dando erro
+                self.canal_atual += 1
 
     def canal_menos(self):
         if self.ligado:
@@ -31,10 +45,14 @@ class ControleRemoto:
                 self.canal_atual -= 1
 
     def volume_mais(self):
-        pass
+        if self.ligado:
+            if self.volume_atual != self.volume_max:
+                self.volume_atual += 1
 
     def volume_menos(self):
-        pass
+        if self.ligado:
+            if self.volume_atual != self.volume_min:
+                self.volume_atual -= 1
 
     def mostrar_tv(self):
 
@@ -44,7 +62,7 @@ class ControleRemoto:
         else:
             conteudo = f"CANAL = "
 
-            for canal in range(self.canal_min, self.canal_max ):
+            for canal in range(self.canal_min, self.canal_max +1 ):
                 if canal == self.canal_atual:       
                     conteudo += f"[yellow on yellow]  {canal} [/] "
                 else: 
@@ -54,14 +72,28 @@ class ControleRemoto:
             
             for volume in range(self.volume_min, self.volume_max +1):
                 if volume <= self.volume_atual:
-                    conteudo += f"[black on yellow]   [/]"
+                    conteudo += "[black on yellow]  [/]"
                 else: 
-                    conteudo += f"[black on white]   [/] "
+                    conteudo += "[black on white]  [/]"
 
-        tv = Panel(conteudo, title="[ TV ]", width=32)
+        tv = Panel(conteudo, title="[ TV ]", width=32, padding=1)
         print(tv)
 
-c = ControleRemoto(canal=1, volume=3)
-c.liga_desliga()
-c.canal_menos()
-c.mostrar_tv()
+c = ControleRemoto()
+
+while True:
+    c.mostrar_tv()
+    comando = str(input(f'< CH{c.canal_atual}>  - VOL{c.volume_atual} + : '))
+    match comando:
+        case '0':
+            break
+        case '@':
+            c.liga_desliga()
+        case '>':
+            c.canal_mais()
+        case '<':
+            c.canal_menos()
+        case '-':
+            c.volume_menos()
+        case '+':
+            c.volume_mais()
