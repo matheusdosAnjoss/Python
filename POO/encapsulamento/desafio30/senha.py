ae1bc1257aab44b16fc76174dd50482af9cc4146
Matheus Dos Anjos Oliveira <132523220+matheusdosAnjoss@users.noreply.github.com>
2026-07-06 20:50:03 -0300

    Att

--- POO/encapsulamento/desafio30/senha.py
+++ POO/encapsulamento/desafio30/senha.py
@@ -1,28 +1,33 @@
 import hashlib
 
 class Credencial:
-    def __init__(self, senha_inicial):
+    def __init__(self, senha_inicial='python'):
         self.__hash = ""
         self.senha = senha_inicial
 
     @property
     def senha(self):
-        return '*******'
+        return self.__hash
     @senha.setter
     def senha(self, nova_senha):
-        self.__hash = hashlib.sha256(nova_senha.encode('utf8')).hexdigest()
+        self.__hash = hashlib.sha256(nova_senha.encode('utf-8')).hexdigest()
 
     @property
     def hash_seguro(self):
         return self.__hash
 
     def validar(self, chave):
-        chave_hash = hashlib.sha256(chave.encode('utf8')).hexdigest()
-
-        return chave_hash == self.__hash
+        chave_hash = hashlib.sha256(chave.encode('utf-8')).hexdigest()
     
-c = Credencial('python')
-print(f'Hash Hexadecimal {c.hash_seguro}')
+        if chave_hash == self.__hash:
+            return 'Senha confere!'
+        else:
+            return "Senha não bate!"
+
+    
+c = Credencial()
+c.senha = str(input('Digiite sua senha: '))
+# print(f'Hash Hexadecimal {c.hash_seguro}')
 print(f'Senha armazenada: {c.senha}')
 
 print(c.validar('python123'))
